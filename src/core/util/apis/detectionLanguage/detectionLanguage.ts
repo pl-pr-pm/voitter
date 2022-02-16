e919e338f400197aa6d667852eb4b3bf7772d575
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-02-16 21:55:21 +0900

    信頼できる言語が検出されなかった場合のロジックを追加

--- src/core/util/apis/detectionLanguage/detectionLanguage.ts
+++ src/core/util/apis/detectionLanguage/detectionLanguage.ts
@@ -2,15 +2,26 @@ import { client } from './lib/detectionLanguageCient';
 
 // 対象の文章の言語を検出する
 // 複数の言語が含まれる場合は、それぞれの言語が検出されるが、最もスコアが高いものを文章の言語とする
-export const detectionLanguage = async (targetText: string) => {
+export const detectionLanguage = async (
+  targetText: string,
+): Promise<string> => {
   if (!targetText) {
     throw new Error('言語検出対象文章を入力してください');
   }
   try {
     const response = await client.detect(targetText);
+
     const langs = response.filter((lang) => lang.isReliable);
-    // 配列が生成されるため、０番目の要素を抽出（要素は一つしかヒットしない）
-    const detectionLang = langs[0].language;
+
+    let detectionLang = process.env.TWEETVOICE_DEFAULT_LANG;
+    // isReliableがtrueとなるものが存在しない場合がある
+    // その時、レスポンスで検出した０番目の言語を検出言語とする
+    if (langs.length === 0) {
+      detectionLang = response[0].language;
+    } else {
+      // 配列が生成されるため、０番目の要素を抽出（要素は一つしかヒットしない）
+      detectionLang = langs[0].language;
+    }
     return detectionLang;
   } catch (e) {
     console.log(e.toString());
