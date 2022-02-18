2b1ad0b9d04ff5f52e5a5db4348838cd9fcfaf5e
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-02-18 19:44:07 +0900

    サービスロジック追加

--- src/core/applicaton/core.service.ts
+++ src/core/applicaton/core.service.ts
@@ -1,22 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { TextToVoice } from '../domain/textToVoice';
 import { TweetRepository } from '../infrastracture/repository/tweet.repository';
+import { SelectTweetDto } from '../interface/dto/select-tweet.dto';
+import { Toptions } from '../domain/type/type';
 @Injectable()
 export class CoreService {
   constructor(
-    private readonly tweetRepository: TweetRepository,
+    private tweetRepository: TweetRepository,
     private textToVoice: TextToVoice,
   ) {}
 
-  run() {
-    this.textToVoice
-      .run('username', {
-        isTranslate: false,
-        isMale: true,
-        isBoth: true,
-      })
-      .then((res) => {
-        console.log(res);
+  async _createTweet(username: string, options: Toptions) {
+    const query = [];
+    const now = new Date().toISOString();
+    const tweets = await this.textToVoice.run(username, options);
+    for (const tweet of tweets) {
+      query.push({
+        insertOne: {
+          document: {
+            username: username,
+            tweetContent: tweet,
+            tweetCreatedAt: tweet.createdAt,
+            createdAt: now,
+            updatedAt: now,
+          },
+        },
       });
+    }
+    this.tweetRepository.createBulkTweet(query);
+    return tweets;
+  }
+
+  async _selectTweet(selectTweetDto: SelectTweetDto) {
+    return await this.tweetRepository.findByUsername(selectTweetDto);
+  }
+
+  async selectTimeLine(selectTweetDto: SelectTweetDto, options: Toptions) {
+    const { username } = selectTweetDto;
+    const timelines = await this._selectTweet({ username: username });
+    if (!timelines) {
+      this._createTweet(username, options);
+    } else if (timelines[0].createdAt) {
+    }
+  }
+
+  async deleteTimeLine(selectTweetDto: SelectTweetDto): Promise<void> {
+    const { username } = selectTweetDto;
   }
 }
--- src/core/infrastracture/scheme/tweet.scheme.ts
+++ src/core/infrastracture/scheme/tweet.scheme.ts
@@ -9,7 +9,7 @@ export class Tweet {
   @Prop({ required: true })
   username: string;
 
-  @Prop({ type: {}, required: true }) // TypeScriptが型を自動推論できない場合は、型を明示する必要がある。また、TypeScriptの型とスキーマタイプは「型」という同じ概念ではないため、純粋に値の型を定義するとエラーとなる。ここでは、厳密に型を指定するのを諦め、オブジェクトとする
+  @Prop({ type: {}, required: true }) // TypeScriptが型を自動推論できない場合は、型を明示する必要がある。また、TypeScriptの型とスキーマタイプは「型」という同じ概念ではないため、純粋に値の型を定義するとエラーとなる。ここでは、厳密に型を指定するのを諦め、オブジェクトとする。
   tweetContent: TretArray | TbothRetArray;
 
   @Prop({ required: true })
