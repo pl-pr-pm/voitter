e2c9d7191046e68c0728a5217ac9912e6dd747c7
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-02-21 15:32:10 +0900

    JWTモジュールの設定

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,19 @@ import { AuthService } from './domain/auth.service';
 import { UserRepository } from './infrastracture/repository/auth.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './infrastracture/shema/user-schema';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    PassportModule.register({ defaultStrategy: 'jwt' }), // defaultの認証方法をjwtとしている
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_KEY,
+      signOptions: {
+        expiresIn: 3600, // 秒
+      },
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService, UserRepository],
