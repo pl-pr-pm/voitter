6c1598d8f55d408a92fd6e91327363d78df6ee65
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-02-22 21:12:33 +0900

    簡易的パフォーマンス計測を削除

--- src/core/domain/textToVoice.ts
+++ src/core/domain/textToVoice.ts
@@ -5,7 +5,7 @@ import { TranslateTweet } from './apis/deepL/translateTweet';
 import { DetectionLanguage } from './apis/detectionLanguage/detectionLanguage';
 import { GetTimeLine } from './apis/twitter/getTimeline';
 import { Toptions, TretArray, TbothRetArray } from './type/type';
-import { performance } from 'perf_hooks';
+
 @Injectable()
 export class TextToVoice {
   logger = new Logger(TextToVoice.name);
@@ -21,17 +21,14 @@ export class TextToVoice {
     let tweetLang = process.env.TWEETVOICE_DEFAULT_LANG;
     try {
       if (options.isTranslate) {
-        const start = performance.now();
         voiceTarget = await this.translateTweet.translateTweet(tweet.text);
-        console.log('translateTweet ', performance.now() - start);
       } else {
         // 翻訳しない場合、ツイートの言語は原文の言語となるため、ツイートの言語を識別する
         // ツイートの言語を識別するが、現状pollyでの音声生成は、対象となるマッピングが多いため日本語・英語 発音のみとする
         // 日本語以外のツイートの場合、英語での発音とする
         // 翻訳する場合、日本語とする
-        const start = performance.now();
+
         tweetLang = await this.detectionLanguage.detectionLanguage(tweet.text);
-        console.log('detectionLanguage ', performance.now() - start);
       }
       // 男性・女性 両方の声で音声を生成する場合
       if (options.isBoth) {
@@ -57,14 +54,13 @@ export class TextToVoice {
         };
         return retObj;
       } else {
-        const start = performance.now();
         const voiceUrl = await this.getTweetVoice.getTweetVoice(
           voiceTarget,
           options.isTranslate,
           options.isMale,
           tweetLang,
         );
-        console.log('getTweetVoice ', performance.now() - start);
+
         const retObj = {
           tweetText: tweet.text,
           createdAt: tweet.created_at,
@@ -83,9 +79,8 @@ export class TextToVoice {
     const retArray: (TretArray | TbothRetArray)[] = []; //複数の型を持つ配列の定義方法（ハマる〜）
 
     try {
-      const start = performance.now();
       const timeline = await this.getTimeLine.getTimeLine(username, 5);
-      console.log('getTimeLine ', performance.now() - start);
+
       for (const tweet of timeline) {
         retArray.push(await this.textToVoiceObj(tweet, options));
       }
