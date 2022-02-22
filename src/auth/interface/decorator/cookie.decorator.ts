24d9fe2018e2125fe95f5bbecd0618c5b1200724
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-02-22 21:07:27 +0900

    発生するエラーを変更

--- src/auth/interface/decorator/cookie.decorator.ts
+++ src/auth/interface/decorator/cookie.decorator.ts
@@ -2,7 +2,6 @@ import {
   BadRequestException,
   createParamDecorator,
   ExecutionContext,
-  HttpStatus,
 } from '@nestjs/common';
 // パラメータにCookie名の指定があれば、指定のCookieの値を返却
 // パラメータにCookie名の指定がなければ、Cookie全てを返却
@@ -11,13 +10,11 @@ import {
 export const Cookies = createParamDecorator(
   (cookieName: string, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
+
     const headers = request.headers;
     const cookies: string[] = headers.cookie?.split(';');
     if (!cookies)
-      throw new BadRequestException({
-        status: HttpStatus.BAD_REQUEST,
-        error: 'Cookieが設定されているか確認してください',
-      });
+      throw new BadRequestException('Cookieが設定されているか確認してください');
 
     if (cookieName === undefined) return cookies;
     for (const cookie of cookies) {
@@ -27,9 +24,6 @@ export const Cookies = createParamDecorator(
         return value;
       }
     }
-    throw new BadRequestException({
-      status: HttpStatus.BAD_REQUEST,
-      error: 'Cookieが設定されているか確認してください',
-    });
+    throw new BadRequestException('Cookieが設定されているか確認してください');
   },
 );
