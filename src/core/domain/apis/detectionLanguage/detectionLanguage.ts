5f0947f44b9bc056abd32c2f01614fab3ae313c5
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-02-22 18:41:43 +0900

    処理時間が長いため、detectionAPIから文字列の正規表現を利用したロジックへと変換

--- src/core/domain/apis/detectionLanguage/detectionLanguage.ts
+++ src/core/domain/apis/detectionLanguage/detectionLanguage.ts
@@ -1,34 +1,38 @@
-import { Injectable } from '@nestjs/common';
-import { client } from './lib/detectionLanguageCient';
+import { BadRequestException, HttpException, Injectable } from '@nestjs/common';
 
 // 対象の文章の言語を検出する
-// 複数の言語が含まれる場合は、それぞれの言語が検出されるが、最もスコアが高いものを文章の言語とする
+// もともとは、detection language API (https://detectlanguage.com/) を利用していたが、
+// １リクエストあたり約１秒かかっていたため、APIを利用するのをやめ、日本語か英語かのみを正規表現を利用し判定することにした
+// 日本語（ひらがな、カタカナ、漢字）が含まれていた場合、日本語と判定。日本語でない場合は英語と判定。
 @Injectable()
 export class DetectionLanguage {
-  detectionLanguage = async (targetText: string): Promise<string> => {
-    if (!targetText) {
-      throw new Error(
-        `言語検出対象文章を入力してください targetText is ${targetText}`,
-      );
-    }
-    try {
-      const response = await client.detect(targetText);
-
-      const langs = response.filter((lang) => lang.isReliable);
-
-      let detectionLang = process.env.TWEETVOICE_DEFAULT_LANG;
-      // isReliableがtrueとなるものが存在しない場合がある
-      // その時、レスポンスで検出した０番目の言語を検出言語とする
-      if (langs.length === 0) {
-        detectionLang = response[0].language;
-      } else {
-        // 配列が生成されるため、０番目の要素を抽出（要素は一つしかヒットしない）
-        detectionLang = langs[0].language;
-      }
-      return detectionLang;
-    } catch (e) {
-      console.log(e.toString());
-      throw new Error(`言語の検出ができませんでした ${e.toString()}`);
-    }
+  isJA = (target: string) => {
+    // \u3040-\u309f ひらがな
+    // \u30a0-\u30ff カタカナ
+    // \u30e0-\u9fcf 漢字
+    return target.match(/[\u3040-\u309f\u30a0-\u30ff\u30e0-\u9fcf]+$/)
+      ? true
+      : false;
   };
+
+  detectionLanguage = async (targetText: string): Promise<string> => {
+    let detectionLang = process.env.TWEETVOICE_DEFAULT_LANG;
+    if (!targetText) {
+      throw new BadRequestException('識別対象文章を確認してください');
+    }
+    if (!this.isJA(targetText)) {
+      detectionLang = process.env.DETECTION_ENGLISH_LANGUAGE;
+    }
+
+    return detectionLang;
+  };
+  catch(e: any) {
+    throw new HttpException(
+      {
+        statusCode: 511,
+        message: `文章の言語識別に失敗しました ${e.message()}`,
+      },
+      511,
+    );
+  }
 }
