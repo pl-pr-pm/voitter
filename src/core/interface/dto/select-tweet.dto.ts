07bcef49f6ddfc9c13bbff38910f88b73969561d
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-02-22 16:31:52 +0900

    usernameの書式を変更

--- src/core/interface/dto/select-tweet.dto.ts
+++ src/core/interface/dto/select-tweet.dto.ts
@@ -1,15 +1,10 @@
-import {
-  IsNotEmpty,
-  IsNumberString,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class SelectTweetDto {
   // twitterの仕様上usernameは、4文字以上50文字以下となる
   @IsNotEmpty()
   @MaxLength(50)
   @MinLength(4)
-  @IsNumberString()
+  @IsString()
   username: string;
 }
