c2342069db73afa85132f6d12daeef3bf0767b63
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-03-26 18:54:05 +0900

    DRY化

--- src/auth/interface/dto/create-user.dto.ts
+++ src/auth/interface/dto/create-user.dto.ts
@@ -1,31 +1,9 @@
-import {
-  IsString,
-  IsNotEmpty,
-  MaxLength,
-  MinLength,
-  IsEnum,
-} from 'class-validator';
+import { IsString, IsEnum } from 'class-validator';
 import { UserStatus } from '../../domain/enum/user-status';
-import { Validate } from 'class-validator';
-import {
-  VoitterUsernameFormat,
-  PasswordFormat,
-} from '../validate/credentials.validate';
 
-export class CreateUserDto {
-  @IsNotEmpty()
-  @IsString()
-  @MaxLength(50)
-  @MinLength(4)
-  @Validate(VoitterUsernameFormat)
-  username: string;
-  @IsNotEmpty()
-  @IsString()
-  @MaxLength(16)
-  @MinLength(8)
-  @Validate(PasswordFormat)
-  password: string;
+import { CredentialsDto } from './credentials.dto';
 
+export class CreateUserDto extends CredentialsDto {
   @IsEnum(UserStatus)
   status: UserStatus;
 
